f0145e35bcb93a9362039d8688814ce622be16ae
Adam Prickett <adam@digivo.co.uk>
2016-05-11 20:49:41 +0100

    Initial commit

---
+++ readme.md
@@ -0,0 +1,37 @@
+## String Blade Compiler
+
+Originally forked from [Flynsarmy/laravel-db-blade-compiler](https://github.com/Flynsarmy/laravel-db-blade-compiler)
+
+### Render Blade templates from a string
+
+This package generates and returns a compiled view from a provided string
+
+### Installation (Laravel 5.x)
+
+Require this package in your composer.json and run composer update (or run `composer require digivo/string-blade-compiler:1.*` directly):
+
+    "flynsarmy/string-blade-compiler": "1.*"
+
+After updating composer, add the ServiceProvider to the providers array in app/config/app.php
+
+    'Digivo\StringBladeCompiler\StringBladeCompilerServiceProvider',
+
+and the Facade to the aliases array in the same file
+
+    'StringView'          => 'Digivo\StringBladeCompiler\Facades\DigivoView',
+
+You have to also publish the config-file
+
+    php artisan vendor:publish
+
+
+### Usage
+
+This package offers a `StringView` facade with the same syntax as `View` but accepts a string instead of path to view.
+
+    return StringView::make('@if ($foo == "Bar") foo is Bar @else foo is not Bar @endif')->with(['foo' => 'Bar'])->render();
+
+
+### License
+
+string-blade-compiler is open-sourced software licensed under the [MIT license](http://opensource.org/licenses/MIT)
---
+++ src/Digivo/StringBladeCompiler/StringBladeCompilerServiceProvider.php
@@ -0,0 +1,67 @@
+<?php 
+namespace Digivo\StringBladeCompiler;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\View\Engines\CompilerEngine;
+
+class StringBladeCompilerServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+	    $config_path = __DIR__ . '/../../../config/string-blade-compiler.php';
+	    $this->publishes([$config_path => config_path('string-blade-compiler.php')], 'config');
+
+	    $views_path = __DIR__ . '/../../../config/.gitkeep';
+	    $this->publishes([$views_path => storage_path('app/string-blade-compiler/views/.gitkeep')]);
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+	    $config_path = __DIR__ . '/../../../config/string-blade-compiler.php';
+	    $this->mergeConfigFrom($config_path, 'string-blade-compiler');
+
+	    $this->app['stringview'] = $this->app->share(function($app)
+	    {
+	        $cache_path = storage_path('app/string-blade-compiler/views');
+
+	        $string_view = new StringView($app['config']);
+	        $compiler = new StringBladeCompiler($app['files'], $cache_path, $app['config'], $app);
+	        $string_view->setEngine(new CompilerEngine($compiler));
+
+	        return $string_view;
+	    });
+	    $this->app->booting(function()
+	    {
+	        $loader = \Illuminate\Foundation\AliasLoader::getInstance();
+	        $loader->alias('StringView', 'Digivo\StringBladeCompiler\Facades\StringView');
+	    });
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
